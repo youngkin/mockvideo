f016343c27b73e984027a69e1ccf7a0057d1d8ff
youngkin <rich.youngkin@gmail.com>
2020-03-07 12:37:12 -0700

    add MySQL endpoint; configure JSON logging

--- internal/platform/logging/logger.go
+++ internal/platform/logging/logger.go
@@ -11,7 +11,7 @@ var logger *log.Entry
 
 func init() {
 	// Log as JSON instead of the default ASCII formatter.
-	log.SetFormatter(&log.TextFormatter{})
+	log.SetFormatter(&log.JSONFormatter{})
 
 	// Output to stdout instead of the default stderr
 	// Can be any io.Writer, see below for File example
